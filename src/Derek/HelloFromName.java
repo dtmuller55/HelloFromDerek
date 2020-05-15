763de0da2bd846b8bfe846d729d1d7570e5e791d
Cellphone104 <53287422+dmuller104@users.noreply.github.com>
2020-05-15 11:04:20 -0600

    Added some stuff

--- src/Derek/HelloFromName.java
+++ src/Derek/HelloFromName.java
@@ -2,12 +2,16 @@ package Derek;
 //A friendly introduction class
 public class HelloFromName {
     public void Hello(final String name){
-        System.out.print("Hello, my name is " + name);
+        System.out.printf("Hello, my name is " + name + "%n");
     }
 
     public static void main(String []args){
         HelloFromName hello = new HelloFromName();
         hello.Hello("Derek");
+    }
+
+    public void HelloNeighbor(final String to, final String from){
+        System.out.printf("Hello " + to + ", my name is " + from + "%n");
     }
 
 }
